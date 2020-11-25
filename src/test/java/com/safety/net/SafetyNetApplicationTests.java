57bd767f626da96c77d0301c3a3dce47b540087c
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2020-11-25 17:30:01 +0100

    create safetyNet app

---
+++ src/test/java/com/safety/net/SafetyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safety.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
