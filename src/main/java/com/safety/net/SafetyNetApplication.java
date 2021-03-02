b458870571e19317d5cf51861d1ed7d05ed99aad
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-03-03 00:46:54 +0100

    AJout d'une methode afin d'afficher du code AScii

--- src/main/java/com/safety/net/SafetyNetApplication.java
+++ src/main/java/com/safety/net/SafetyNetApplication.java
@@ -3,20 +3,31 @@ package com.safety.net;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
+
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.safety.net.services.DisplayApiAscii;
+
 @SpringBootApplication
 @ComponentScan
 public class SafetyNetApplication {
-	
+
+	@Autowired
+	DisplayApiAscii displayApiAscii;
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
 		SpringApplication.run(SafetyNetApplication.class, args);
-		
 
+	}
+	
+	@PostConstruct
+	public void asciiSysOut() {
+		displayApiAscii.displayAscii();
 	}
 
 }
