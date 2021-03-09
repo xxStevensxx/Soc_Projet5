822a1c4487a7ce840f4a4fa5f40e500f160a3c08
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-03-09 20:02:15 +0100

    Optimisations des divers controllers

--- src/main/java/com/safety/net/controller/FireStationsController.java
+++ src/main/java/com/safety/net/controller/FireStationsController.java
@@ -1,39 +1,177 @@
 package com.safety.net.controller;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.safety.net.model.Address;
 import com.safety.net.model.FireStations;
 import com.safety.net.model.ListObject;
 import com.safety.net.services.AddressServices;
 import com.safety.net.services.FireStationsServices;
+import com.safety.net.util.CentralizedMsg;
+import com.safety.net.util.FilterJcksn;
 
-
+@RestController
 public class FireStationsController {
-		
+
 	@Autowired
 	FireStationsServices fireStationServices;
 
 	@Autowired
 	AddressServices adrServices;
-	
 
-	
-	@RequestMapping(value = "/person", method = RequestMethod.POST, produces = "application/json")
-	public ResponseEntity<List<FireStations>> addFireStations(@RequestBody FireStations fireStation){
-		
-		fireStationServices.addFireStations(fireStation);
-		
-		adrServices.addAddress(fireStation.getAddress());
-		
-		return new ResponseEntity<>(ListObject.listFireStations, HttpStatus.OK);
-		
+	@Autowired
+	FilterJcksn filterJcksn;
+
+	@Autowired
+	CentralizedMsg msg;
+
+	ArrayList<FireStations> fireStations;
+	boolean existe;
+	String fireAdr;
+	Address adr;
+	int stationList;
+
+	@RequestMapping(value = "/firestationAPI", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<MappingJacksonValue> addFireStations(@RequestBody FireStations fireStation) {
+
+		fireStations = new ArrayList<>();
+
+		for (int iterator = 0; iterator < ListObject.listFireStations.size(); iterator++) {
+
+			existe = false;
+
+			fireStations.add(ListObject.listFireStations.get(iterator));
+
+			if (fireStation.getAddress().getAddress() == null || fireStation.getAddress().getCity() == null
+					|| fireStation.getAddress().getZip() == null) {
+
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(6)),
+						HttpStatus.BAD_REQUEST);
+
+			} else {
+
+				fireAdr = ListObject.listFireStations.get(iterator).getAddress().getAddress().toLowerCase()
+						.replaceAll("//s", "");
+
+			}
+
+			if (fireStation.getStation() == fireStations.get(iterator).getStation()
+					&& fireStation.getAddress().getAddress().toLowerCase().replaceAll("//s", "").contains(fireAdr)) {
+
+				existe = true;
+
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(4)),
+						HttpStatus.CONFLICT);
+
+			}
+
+		}
+
+		if (existe == false) {
+
+			if (fireStation.getAddress() != null) {
+
+				adrServices.addAddress(fireStation.getAddress());
+
+			}
+
+			fireStationServices.addFireStations(fireStation);
+			adrServices.addAddress(fireStation.getAddress());
+
+			fireStations.add(fireStation);
+
+		}
+
+		return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterFire(fireStations), HttpStatus.CREATED);
+
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/AllFireStations", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<MappingJacksonValue> readAllFireStations() {
+
+		return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterFire(ListObject.listFireStations),
+				HttpStatus.FOUND);
+
+	}
+
+	@RequestMapping(value = "/firestationUpdate", method = RequestMethod.PUT, produces = "application/json")
+	public ResponseEntity<MappingJacksonValue> updateFireStations(@RequestBody FireStations fireStation,
+			@RequestParam(name = "station", required = true) int station,
+			@RequestParam(name = "address", required = true) String address) {
+
+		boolean adrIsNull;
+
+		for (int iterator = 0; iterator < ListObject.listFireStations.size(); iterator++) {
+
+			adrIsNull = false;
+
+			fireStations = new ArrayList<>();
+			fireStations.add(ListObject.listFireStations.get(iterator));
+
+			fireAdr = ListObject.listFireStations.get(iterator).getAddress().getAddress().toLowerCase();
+			stationList = ListObject.listFireStations.get(iterator).getStation();
+
+			if (address == null || address.isEmpty()) {
+
+				adrIsNull = true;
+
+			}
+
+			if (adrIsNull == false && station == stationList
+					&& address.toLowerCase().replaceAll("//s", "").contains(fireAdr)) {
+
+				fireStationServices.updateFireStation(fireStation, iterator);
+
+				return new ResponseEntity<MappingJacksonValue>(
+						filterJcksn.genericFilterFire(ListObject.listFireStations), HttpStatus.OK);
+
+			}
+		}
+
+		return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(5)), HttpStatus.NOT_FOUND);
+
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/deleteFireStation", method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity<MappingJacksonValue> deletePersons(
+			@RequestParam(name = "station", required = true) int station,
+			@RequestParam(name = "address", required = true) String address) {
+
+		for (int iterator = 0; iterator < ListObject.listPersons.size(); iterator++) {
+
+			fireStations = new ArrayList<>();
+
+			fireStations.add(ListObject.listFireStations.get(iterator));
+
+			int stationIterator = ListObject.listFireStations.get(iterator).getStation();
+			String adrIterator = ListObject.listFireStations.get(iterator).getAddress().getAddress().toLowerCase();
+
+			if (stationIterator == station && adrIterator.contains(address.toLowerCase().replaceAll("\\s", ""))) {
+
+				fireStationServices.removeFireStation(iterator);
+
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterFire(ListObject.listFireStations),
+						HttpStatus.ACCEPTED);
+
+			}
+
+		}
+
+		return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(5)), HttpStatus.NOT_FOUND);
+
 	}
 
 }
--- src/main/java/com/safety/net/controller/PersonsController.java
+++ src/main/java/com/safety/net/controller/PersonsController.java
@@ -36,12 +36,10 @@ public class PersonsController {
 
 	@Autowired
 	CentralizedMsg msg;
-	
+
 	@Autowired
 	DateManager dateManager;
-	
 
-	
 	ArrayList<Persons> persons;
 	Persons personUpdate;
 	String firstName;
@@ -54,11 +52,11 @@ public class PersonsController {
 	
 	
 	
+
 	@RequestMapping(value = "/person", method = RequestMethod.POST, produces = "application/json")
 	public ResponseEntity<MappingJacksonValue> addPersons(@RequestBody Persons person) {
 
 		persons = new ArrayList<>();
-		
 
 		for (int i = 0; i < ListObject.listPersons.size(); i++) {
 
@@ -70,33 +68,35 @@ public class PersonsController {
 			lastName = ListObject.listPersons.get(i).getLastName().toLowerCase();
 			email = ListObject.listPersons.get(i).getEmail().toLowerCase();
 
-			if (firstName.contains(person.getFirstName().toLowerCase().replaceAll("//s", "")) && lastName.contains(person.getLastName().toLowerCase().replaceAll("//s", ""))) {
-				
-				existe = true;
-				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(1)), HttpStatus.CONFLICT);
+			if (firstName.contains(person.getFirstName().toLowerCase().replaceAll("//s", ""))
+					&& lastName.contains(person.getLastName().toLowerCase().replaceAll("//s", ""))) {
 
-			}else if (email.contains(person.getEmail().toLowerCase().replaceAll("//s", ""))) {
-				
 				existe = true;
-				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(2)), HttpStatus.CONFLICT);
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(1)),
+						HttpStatus.CONFLICT);
+
+			} else if (email.contains(person.getEmail().toLowerCase().replaceAll("//s", ""))) {
+
+				existe = true;
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(2)),
+						HttpStatus.CONFLICT);
 
 			}
 
 		}
 
-		if (existe == false ) {
-			
-			if(person.getBirthDate() != null) {
-								
-				
-				date = person.getBirthDate();	
+		if (existe == false) {
+
+			if (person.getBirthDate() != null) {
+
+				date = person.getBirthDate();
 				age = dateManager.calculateAge(date.toString());
-				
+
 				person.setAge(Integer.parseInt(age));
 				person.setBirthDate(date);
-				
+
 			}
-			
+
 			personServices.addPerson(person);
 
 			adrServices.addAddress(person.getLocation());
@@ -104,42 +104,42 @@ public class PersonsController {
 			persons.add(person);
 		}
 
-		return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterCRUD(persons), HttpStatus.CREATED);
+		return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterPerson(persons), HttpStatus.CREATED);
 
 	}
-	
-	
-	
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/AllPersons", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<MappingJacksonValue> readAllPersons() {
-		
-		
-		return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilter(ListObject.listPersons), HttpStatus.FOUND);
+
+		return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterPerson(ListObject.listPersons),
+				HttpStatus.FOUND);
 
 	}
 	
 	
+	
+	
+
 	@ResponseBody
 	@RequestMapping(value = "/person", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<MappingJacksonValue> readPersons(
-			@RequestParam( name = "firstName", required = true) String frstName) {
-		
+			@RequestParam(name = "firstName", required = true) String frstName) {
+
 		persons = new ArrayList<>();
-		
+
 		for (int i = 0; i < ListObject.listPersons.size(); i++) {
-			
-			if (ListObject.listPersons.get(i).getFirstName().toLowerCase().equals(frstName.toLowerCase().replaceAll("//s", ""))) {
+
+			if (ListObject.listPersons.get(i).getFirstName().toLowerCase()
+					.equals(frstName.toLowerCase().replaceAll("//s", ""))) {
 
 				persons.add(ListObject.listPersons.get(i));
-				
-				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterCRUD(persons), HttpStatus.FOUND);
+
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterPerson(persons), HttpStatus.FOUND);
 
 			}
-			
+
 		}
-		
 
 		return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(3)), HttpStatus.NOT_FOUND);
 
@@ -148,46 +148,76 @@ public class PersonsController {
 	
 	
 	
-	
+
 	@RequestMapping(value = "/personUpdate", method = RequestMethod.PUT, produces = "application/json")
-	public ResponseEntity<MappingJacksonValue> updatePersons(@RequestBody Persons person, 
-			@RequestParam( name = "firstName", required = true) String frstName, @RequestParam(name = "lastName", required = true) String lstName) {
-		
-		
+	public ResponseEntity<MappingJacksonValue> updatePersons(@RequestBody Persons person,
+			@RequestParam(name = "firstName", required = true) String frstName,
+			@RequestParam(name = "lastName", required = true) String lstName) {
+
 		for (int i = 0; i < ListObject.listPersons.size(); i++) {
-			
+
 			persons = new ArrayList<>();
 
 			persons.add(ListObject.listPersons.get(i));
-			
 
 			firstName = ListObject.listPersons.get(i).getFirstName().toLowerCase();
 			lastName = ListObject.listPersons.get(i).getLastName().toLowerCase();
 			email = ListObject.listPersons.get(i).getEmail().toLowerCase();
-			
-			if (person.getEmail().toLowerCase().replaceAll("//s", "").contains(email)) {
-				
-				return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(2)), HttpStatus.CONFLICT);
 
-			}
+	
 
-			if (firstName.contains(frstName.toLowerCase().replaceAll("\\s", "")) && 
-					lastName.contains(lstName.toLowerCase().replaceAll("\\s", ""))) {
+			if (firstName.contains(frstName.toLowerCase().replaceAll("\\s", ""))
+					&& lastName.contains(lstName.toLowerCase().replaceAll("\\s", ""))) {
 
+//				personUpdate = new Persons();
+//
+//				personUpdate = ListObject.listPersons.get(i);
 				
-				personUpdate = new Persons();
-				
-				personUpdate = ListObject.listPersons.get(i);
 				personServices.updatePerson(person, i);
-				
-				return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilter(ListObject.listPersons), HttpStatus.OK);
 
-				
-				
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterPerson(ListObject.listPersons),
+						HttpStatus.OK);
+
 			}
 
 		}
-		
+
+		return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(3)), HttpStatus.NOT_FOUND);
+
+	}
+	
+	
+	
+	
+
+	@ResponseBody
+	@RequestMapping(value = "/deletePerson", method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity<MappingJacksonValue> deletePersons(
+			@RequestParam(name = "firstName", required = true) String frstName,
+			@RequestParam(name = "lastName", required = true) String lstName) {
+
+		for (int iterator = 0; iterator < ListObject.listPersons.size(); iterator++) {
+
+			persons = new ArrayList<>();
+
+			persons.add(ListObject.listPersons.get(iterator));
+
+			firstName = ListObject.listPersons.get(iterator).getFirstName().toLowerCase();
+			lastName = ListObject.listPersons.get(iterator).getLastName().toLowerCase();
+
+			if (firstName.contains(frstName.toLowerCase().replaceAll("\\s", ""))
+					&& lastName.contains(lstName.toLowerCase().replaceAll("\\s", ""))) {
+				
+				
+				personServices.removePerson(iterator);
+
+
+				return new ResponseEntity<MappingJacksonValue>(filterJcksn.genericFilterPerson(ListObject.listPersons),
+						HttpStatus.ACCEPTED);
+
+			}
+
+		}
 
 		return new ResponseEntity<MappingJacksonValue>(filterJcksn.filterMsg(msg.msgManager(3)), HttpStatus.NOT_FOUND);
 
