34e31bc2c17bb1582008b57a7b6abe3231c9d747
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-03-16 18:46:33 +0100

    Ajout du package model de test

---
+++ src/test/java/com/safety/net/model/ListObjectTest.java
@@ -0,0 +1,13 @@
+package com.safety.net.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListObjectTest {
+
+	public static List<Persons> listPersonsTest = new ArrayList<>();
+	public static List<FireStations> listFireStationsTest = new ArrayList<>();
+	public static List<Address> listAddressTest = new ArrayList<>();
+	public static List<MedicalRecords> listMedicalRecordsTest = new ArrayList<>();
+
+}
